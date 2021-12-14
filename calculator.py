ced4ca32e97cba0016c6178dd02b47cbbe0aa8bc
lonaslee <93890846+lonaslee@users.noreply.github.com>
2021-12-13 21:21:28 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,13 @@
-def GetInputs():
+def GetInputs() -> tuple:
     in1 = CleanInputs(input('A:\n'))
     in2 = CleanInputs(input('B:\n'))
     in3 = CleanInputs(input('C:\n'))
     if len(in1) != 2 or len(in2) != 2 or len(in3) != 2:
         raise IndexError('the inputs are not correctly formatted')
-    in4 = input('o=orthocenter, ce=centroid, ci=circumcenter, a=all\n').strip()
     a = {'x':float(in1[0]), 'y':float(in1[1])}
     b = {'x':float(in2[0]), 'y':float(in2[1])}
     c = {'x':float(in3[0]), 'y':float(in3[1])}
-    return (a, b, c, in4)
+    return (a, b, c)
 
 def CleanInputs(inp) -> list:
     inp = inp.strip('()')
@@ -126,48 +125,101 @@ def Clean(dusty: str) -> str:
 def DeciMate(num) -> float:
     return round(num, 3)
 
-def Coords(Point='O', **kpoints):
+def Coords(Point='O', **kpoints) -> tuple:
     if Point == 'O':
-        FlatLine = 'A' if kpoints['eq1']['pr'] == f'y={kpoints["a"]["y"]}' else 'B' if kpoints['eq2']['pr'] == f'y={kpoints["b"]["y"]}' else 'C' if kpoints['eq3']['pr'] == f'y={kpoints["c"]["y"]}' else None
-        TallLine = 'A' if kpoints['eq1']['pr'] == f'x={kpoints["a"]["x"]}' else 'B' if kpoints['eq2']['pr'] == f'x={kpoints["b"]["x"]}' else 'C' if kpoints['eq3']['pr'] == f'x={kpoints["c"]["x"]}' else None
+        hLine = (
+            'A' if kpoints['eq1']['pr'] == f'y={kpoints["a"]["y"]}' else
+            'B' if kpoints['eq2']['pr'] == f'y={kpoints["b"]["y"]}' else
+            'C' if kpoints['eq3']['pr'] == f'y={kpoints["c"]["y"]}' else None
+        )
+        vLine = (
+            'A' if kpoints['eq1']['pr'] == f'x={kpoints["a"]["x"]}' else
+            'B' if kpoints['eq2']['pr'] == f'x={kpoints["b"]["x"]}' else
+            'C' if kpoints['eq3']['pr'] == f'x={kpoints["c"]["x"]}' else None
+        )
     else:
-        FlatLine = 'A' if 'x+' not in kpoints['eq1']['pr'] and 'x-' not in kpoints['eq1']['pr'] and 'y=' in kpoints['eq1']['pr'] else 'B' if 'x+' not in kpoints['eq2']['pr'] and 'x-' not in kpoints['eq2']['pr'] and 'y=' in kpoints['eq2']['pr'] else 'C' if 'x+' not in kpoints['eq3']['pr'] and 'x-' not in kpoints['eq3']['pr'] and 'y=' in kpoints['eq3']['pr'] else None
-        TallLine = 'A' if 'x=' in kpoints['eq1']['pr'] else 'B' if 'x=' in kpoints['eq2']['pr'] else 'C' if 'x=' in kpoints['eq3']['pr'] else None
-    if FlatLine == None:
-        if TallLine == None: # straight to solve
+        hLine = (
+            'A' if 'x+' not in kpoints['eq1']['pr']
+                and 'x-' not in kpoints['eq1']['pr']
+                and 'y=' in kpoints['eq1']['pr'] else
+            'B' if 'x+' not in kpoints['eq2']['pr']
+                and 'x-' not in kpoints['eq2']['pr']
+                and 'y=' in kpoints['eq2']['pr'] else
+            'C' if 'x+' not in kpoints['eq3']['pr']
+                and 'x-' not in kpoints['eq3']['pr']
+                and 'y=' in kpoints['eq3']['pr'] else None
+        )
+        vLine = (
+            'A' if 'x=' in kpoints['eq1']['pr'] else
+            'B' if 'x=' in kpoints['eq2']['pr'] else
+            'C' if 'x=' in kpoints['eq3']['pr'] else None
+        )
+    if hLine == None:
+        if vLine == None: # straight to solve
             Solve1 = Solve(kpoints['eq1']['eq'], kpoints['eq2']['eq'])
             Solve2 = Solve(kpoints['eq2']['eq'], kpoints['eq3']['eq'])
             Solve3 = Solve(kpoints['eq3']['eq'], kpoints['eq1']['eq'])
-            return (Average(Solve1[0], Solve2[0], Solve3[0]), Average(Solve1[1], Solve2[1], Solve3[1]))
-        elif TallLine == 'A': # solve with B and C
+            return (Average(Solve1[0], Solve2[0], Solve3[0]), 
+                    Average(Solve1[1], Solve2[1], Solve3[1]))
+        elif vLine == 'A': # solve with B and C
             return (Solve(kpoints['eq2']['eq'], kpoints['eq3']['eq']))
-        elif TallLine == 'B': # sw A C
+        elif vLine == 'B': # sw A C
             return (Solve(kpoints['eq1']['eq'], kpoints['eq3']['eq']))
         else: # 'C' sw A B
             return (Solve(kpoints['eq1']['eq'], kpoints['eq2']['eq']))
-    elif FlatLine == 'A':
-        if TallLine == None:
+    elif hLine == 'A':
+        if vLine == None:
             return (Solve(kpoints['eq2']['eq'], kpoints['eq3']['eq']))
-        elif TallLine == 'B': # y of A, x of B
-            return (DeciMate(kpoints['b']['x']), DeciMate(kpoints['a']['y'])) if Point == 'O' else (DeciMate(float(kpoints['eq2']['eq'])), DeciMate(float(kpoints['eq1']['eq'])))
+        elif vLine == 'B': # y of A, x of B
+            return (
+                DeciMate(kpoints['b']['x']), DeciMate(kpoints['a']['y'])
+                if Point == 'O' else 
+                DeciMate(float(kpoints['eq2']['eq'])),
+                DeciMate(float(kpoints['eq1']['eq']))
+            )
         else: # 'C'
-            return (DeciMate(kpoints['c']['x']), DeciMate(kpoints['a']['y'])) if Point == 'O' else (DeciMate(float(kpoints['eq3']['eq'])), DeciMate(float(kpoints['eq1']['eq'])))
-    elif FlatLine == 'B':
-        if TallLine == None:
-            return (Solve(kpoints['eq1']['eq'], kpoints['eq3']['eq']))        
-        elif TallLine == 'A': # y of b
-            return (DeciMate(kpoints['a']['x']), DeciMate(kpoints['b']['y'])) if Point == 'O' else (DeciMate(float(kpoints['eq1']['eq'])), DeciMate(float(kpoints['eq2']['eq'])))
+            return (
+                DeciMate(kpoints['c']['x']), DeciMate(kpoints['a']['y'])
+                if Point == 'O' else
+                DeciMate(float(kpoints['eq3']['eq'])),
+                DeciMate(float(kpoints['eq1']['eq']))
+            )
+    elif hLine == 'B':
+        if vLine == None:
+            return Solve(kpoints['eq1']['eq'], kpoints['eq3']['eq'])
+        elif vLine == 'A': # y of b
+            return (
+                DeciMate(kpoints['a']['x']), DeciMate(kpoints['b']['y'])
+                if Point == 'O' else
+                DeciMate(float(kpoints['eq1']['eq'])),
+                DeciMate(float(kpoints['eq2']['eq']))
+            )
         else: # 'C'
-            return (DeciMate(kpoints['c']['x']), DeciMate(kpoints['b']['y'])) if Point == 'O' else (DeciMate(float(kpoints['eq3']['eq'])), DeciMate(float(kpoints['eq2']['eq'])))
+            return (
+                DeciMate(kpoints['c']['x']), DeciMate(kpoints['b']['y'])
+                if Point == 'O' else 
+                DeciMate(float(kpoints['eq3']['eq'])),
+                DeciMate(float(kpoints['eq2']['eq']))
+            )
     else: # 'C'
-        if TallLine == None:
-            return (Solve(kpoints['eq1']['eq'], kpoints['eq2']['eq']))
-        elif TallLine == 'A': # y of c
-            return (DeciMate(kpoints['a']['x']), DeciMate(kpoints['c']['y'])) if Point == 'O' else (DeciMate(float(kpoints['eq1']['eq'])), DeciMate(float(kpoints['eq3']['eq'])))
+        if vLine == None:
+            return Solve(kpoints['eq1']['eq'], kpoints['eq2']['eq'])
+        elif vLine == 'A': # y of c
+            return (
+                DeciMate(kpoints['a']['x']), DeciMate(kpoints['c']['y'])
+                if Point == 'O' else 
+                DeciMate(float(kpoints['eq1']['eq'])),
+                DeciMate(float(kpoints['eq3']['eq']))
+            )
         else: # 'B'
-            return (DeciMate(kpoints['b']['x']), DeciMate(kpoints['c']['y'])) if Point == 'O' else (DeciMate(float(kpoints['eq2']['eq'])), DeciMate(float(kpoints['eq3']['eq'])))
+            return (
+                DeciMate(kpoints['b']['x']), DeciMate(kpoints['c']['y'])
+                if Point == 'O' else
+                DeciMate(float(kpoints['eq2']['eq'])),
+                DeciMate(float(kpoints['eq3']['eq']))
+            )
 
-def SlopeEquation(m, x1, y1, per=False):
+def SlopeEquation(m, x1, y1, per=False) -> dict:
     if m == '':
         ForPrint, ForEq = f'x={x1}', f'{x1}'
     elif m == 0:
@@ -175,46 +227,67 @@ def SlopeEquation(m, x1, y1, per=False):
     else:
         m = (m*-1)**-1 if per == False else m # perpendicular
         if m*x1*-1 + y1 < 0:
-            ForPrint, ForEq = f'y={DeciMate(m)}x{DeciMate(m*x1*-1+y1)}', f'{m}x{m*x1*-1+y1}'
+            ForPrint = f'y={DeciMate(m)}x{DeciMate(m*x1*-1+y1)}'
+            ForEq = f'{m}x{m*x1*-1+y1}'
         else:
-            ForPrint, ForEq = f'y={DeciMate(m)}x+{DeciMate(m*x1*-1+y1)}', f'{m}x+{m*x1*-1+y1}'
+            ForPrint = f'y={DeciMate(m)}x+{DeciMate(m*x1*-1+y1)}'
+            ForEq = f'{m}x+{m*x1*-1+y1}'
     ForPrint, ForEq = Clean(ForPrint), Clean(ForEq)
     return {'pr':ForPrint, 'eq':ForEq}
 
-def Orthocenter(a, b, c):
+def Orthocenter(a, b, c) -> str:
     AltA = SlopeEquation(Slope(b['x'], b['y'], c['x'], c['y']), a['x'], a['y'])
     AltB = SlopeEquation(Slope(a['x'], a['y'], c['x'], c['y']), b['x'], b['y'])
     AltC = SlopeEquation(Slope(a['x'], a['y'], b['x'], b['y']), c['x'], c['y'])
     Ortho = Coords(eq1=AltA, eq2=AltB, eq3=AltC, a=a, b=b, c=c)
-    return f'Altitude A: {AltA["pr"]}\nAltitude B: {AltB["pr"]}\nAltitude C: {AltC["pr"]}\nOrthocenter: {Ortho}\n\n'
+    return (
+        f'Altitude A: {AltA["pr"]}\nAltitude B: {AltB["pr"]}\n'
+        f'Altitude C: {AltC["pr"]}\nOrthocenter: {Ortho}\n\n'
+    )
 
-def Circumcenter(a, b, c):
-    PerA = SlopeEquation(Slope(b['x'], b['y'], c['x'], c['y']), Average(b['x'], c['x']), Average(b['y'], c['y']))
-    PerB = SlopeEquation(Slope(a['x'], a['y'], c['x'], c['y']), Average(a['x'], c['x']), Average(a['y'], c['y']))
-    PerC = SlopeEquation(Slope(a['x'], a['y'], b['x'], b['y']), Average(a['x'], b['x']), Average(a['y'], b['y']))
+def Circumcenter(a, b, c) -> str:
+    PerA = (SlopeEquation(Slope(b['x'], b['y'], c['x'], c['y']),
+            Average(b['x'], c['x']), Average(b['y'], c['y'])))
+    PerB = (SlopeEquation(Slope(a['x'], a['y'], c['x'], c['y']),
+            Average(a['x'], c['x']), Average(a['y'], c['y'])))
+    PerC = (SlopeEquation(Slope(a['x'], a['y'], b['x'], b['y']),
+            Average(a['x'], b['x']), Average(a['y'], b['y'])))
     Circum = Coords('C', eq1=PerA, eq2=PerB, eq3=PerC, a=a, b=b, c=c)
-    return f'Perpendicular Bisector of AB: {PerC["pr"]}\nPerpendicular Bisector of AC: {PerB["pr"]}\nPerpendicular Bisector of BC: {PerA["pr"]}\nCircumcenter: {Circum}\n\n'
+    return (
+        f'Perpendicular Bisector of AB: {PerC["pr"]}\n'
+        f'Perpendicular Bisector of AC: {PerB["pr"]}\n'
+        f'Perpendicular Bisector of BC: {PerA["pr"]}\n'
+        f'Circumcenter: {Circum}\n\n'
+    )
 
-def Centroid(a, b, c):
-    MedA = SlopeEquation(Slope((b["x"]+c["x"])/2, (b["y"]+c["y"])/2, a["x"], a["y"]), a['x'], a['y'], True)
-    MedB = SlopeEquation(Slope((a["x"]+c["x"])/2, (a["y"]+c["y"])/2, b["x"], b["y"]), b['x'], b['y'], True)
-    MedC = SlopeEquation(Slope((a["x"]+b["x"])/2, (a["y"]+b["y"])/2, c["x"], c["y"]), c['x'], c['y'], True)
+def Centroid(a, b, c) -> str:
+    MedA = (SlopeEquation(Slope((b["x"]+c["x"])/2, (b["y"]+c["y"])/2,
+            a["x"], a["y"]), a['x'], a['y'], True))
+    MedB = (SlopeEquation(Slope((a["x"]+c["x"])/2, (a["y"]+c["y"])/2,
+            b["x"], b["y"]), b['x'], b['y'], True))
+    MedC = (SlopeEquation(Slope((a["x"]+b["x"])/2, (a["y"]+b["y"])/2,
+            c["x"], c["y"]), c['x'], c['y'], True))
     Centro = (Average(a['x'], b['x'], c['x']), Average(a['y'], b['y'], c['y']))
-    return f'Median A: {MedA["pr"]}\nMedian B: {MedB["pr"]}\nMedian C: {MedC["pr"]}\nCentroid: {Centro}'
+    return (
+        f'Median A: {MedA["pr"]}\nMedian B: {MedB["pr"]}\n'
+        f'Median C: {MedC["pr"]}\nCentroid: {Centro}'
+    )
 
-def Execute():
-    a, b, c, centype = GetInputs()
-    FinalString = f'A({a["x"]}, {a["y"]}) B({b["x"]}, {b["y"]}) C({c["x"]}, {c["y"]})\nSlope of AB: {SlopeEquation(Slope(a["x"], a["y"], b["x"], b["y"]), a["x"], a["y"], True)["pr"]}\nSlope of AC: {SlopeEquation(Slope(a["x"], a["y"], c["x"], c["y"]), a["x"], a["y"], True)["pr"]}\nSlope of BC: {SlopeEquation(Slope(b["x"], b["y"], c["x"], c["y"]), b["x"], b["y"], True)["pr"]}\n\n'
-    if centype == '':
-        FinalString = FinalString+Orthocenter(a, b, c)+Circumcenter(a, b, c)+Centroid(a, b, c)
-    elif centype.upper().startswith('CE'):
-        FinalString = FinalString+Centroid(a, b, c)
-    elif 'C' == centype.upper()[0]:
-        FinalString = FinalString+Circumcenter(a, b, c)
-    elif 'O' == centype.upper()[0]:
-        FinalString = FinalString+Orthocenter(a, b, c)
-    else:
-        FinalString = FinalString+Orthocenter(a, b, c)+Circumcenter(a, b, c)+Centroid(a, b, c)
+def Execute() -> str:
+    a, b, c = GetInputs()
+    ABSlope = (SlopeEquation(Slope(a["x"], a["y"], b["x"], b["y"]),
+                a["x"], a["y"], True)["pr"])
+    ACSlope = (SlopeEquation(Slope(a["x"], a["y"], c["x"], c["y"]),
+                a["x"], a["y"], True)["pr"])
+    BCSlope = (SlopeEquation(Slope(b["x"], b["y"], c["x"], c["y"]),
+                b["x"], b["y"], True)["pr"])
+    FinalString = (
+        f'A({a["x"]}, {a["y"]}) B({b["x"]}, {b["y"]}) C({c["x"]}, {c["y"]})\n'
+        f'Slope of AB: {ABSlope}\nSlope of AC: {ACSlope}\n'
+        f'Slope of BC: {BCSlope}\n\n'
+    )
+    for cent in (Orthocenter, Circumcenter, Centroid):
+        FinalString = FinalString+cent(a, b, c)
     return FinalString
 
 print(Execute())
